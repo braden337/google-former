6af891290286cfa999b7d013202ce6a120b0f7df
Braden Jonsson <braden337@gmail.com>
2020-12-04 20:36:43 -0600

    Change variable names for demo

--- src/main.js
+++ src/main.js
@@ -21,30 +21,30 @@ async function getForm(event) {
     });
 
     let body = await response.text();
-    let googleFormsDocument = new DOMParser().parseFromString(
+    let document = new DOMParser().parseFromString(
       body,
       'text/html'
     );
 
-    let action = googleFormsDocument.querySelector('form').action;
+    let action = document.querySelector('form').action;
 
-    let fields = [...googleFormsDocument.querySelectorAll('[data-params]')]
+    let fields = [...document.querySelectorAll('[data-params]')]
       .map(x => x.dataset.params.match(/"([\w\s]+)".+\[(\d+)/).slice(1))
       .map(([title, id]) => ({title, name: `entry.${id}`}));
 
     if (fields.length > 0) {
-      let your_new_form = `<form action="${action}">\n`;
+      let yourForm = `<form action="${action}">\n`;
 
       for (let field of fields) {
-        your_new_form += `  <label>${field.title}</label>\n`;
-        your_new_form += `  <input type="text" name="${field.name}" required />\n\n`;
+        yourForm += `  <label>${field.title}</label>\n`;
+        yourForm += `  <input type="text" name="${field.name}" required />\n\n`;
       }
 
-      your_new_form += '  <input type="submit" value="Send" />\n';
-      your_new_form += '</form>';
+      yourForm += '  <input type="submit" value="Send" />\n';
+      yourForm += '</form>';
 
       let pre = document.createElement('pre');
-      pre.innerText = your_new_form;
+      pre.innerText = yourForm;
       form.replaceWith(pre);
     } else {
       err();
